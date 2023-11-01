ba6102badfbe5e56ecddb7207207bd3664e8bff7
rosol111 <gulamrosul037@gmail.com>
2023-11-01 19:44:52 +0600

    Added extract booking service data

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db("carDoctor").collection("services");
+      const serviceCollection = client.db("carDoctor").collection("services");
+      const bookingCollection = client.db('carDoctor').collection('bookings');
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -47,6 +48,11 @@ async function run() {
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
+      
+      //   bookings
+      app.post('/bookings', async (req, res) => {
+          const booking = req.body;
+      });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
