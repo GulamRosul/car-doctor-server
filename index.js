2ab12bd7a3411953d4b6fde46b64a2f3f86164fe
rosol111 <gulamrosul037@gmail.com>
2023-11-03 00:42:43 +0600

    Added Use query parameter to load user specific data

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
 
       const options = {
-        projection: { title: 1, price: 1, _id: 1 },
+        projection: { title: 1, price: 1, _id: 1, img: 1 },
       };
 
       const result = await serviceCollection.findOne(query, options);
@@ -47,6 +47,17 @@ async function run() {
     });
 
     //   bookings
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
