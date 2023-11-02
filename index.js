ba3cbe9fa9822acfe8b87e0f5dbcef168eed9e9e
rosol111 <gulamrosul037@gmail.com>
2023-11-02 18:48:40 +0600

    Added save order on the order collection using post method

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-      const serviceCollection = client.db("carDoctor").collection("services");
-      const bookingCollection = client.db('carDoctor').collection('bookings');
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -36,23 +36,23 @@ async function run() {
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        
-        const options = {
-            
-            projection: {title: 1, price: 1, _id: 1 },
-            
+      const query = { _id: new ObjectId(id) };
 
-        };
+      const options = {
+        projection: { title: 1, price: 1, _id: 1 },
+      };
 
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
-      
-      //   bookings
-      app.post('/bookings', async (req, res) => {
-          const booking = req.body;
-      });
+
+    //   bookings
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
