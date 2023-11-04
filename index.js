f371b04b1b1d9a1ccb0d9e45447d63b2dd0f8c4e
rosol111 <gulamrosul037@gmail.com>
2023-11-05 01:17:33 +0600

    Added send token server to client and client to the server side

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.3aeouf4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,14 +40,15 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user,  process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       res
-        .cookie('token', token, {
+        .cookie("token", token, {
           httpOnly: true,
-          secure: false, 
-          sameSite: 'none'
+          secure: false,
         })
-        .send({success: true});
+        .send({ success: true });
     });
 
     //   services related api
@@ -66,6 +74,7 @@ async function run() {
 
     app.get("/bookings", async (req, res) => {
       console.log(req.query.email);
+      console.log("tok tok token", req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
